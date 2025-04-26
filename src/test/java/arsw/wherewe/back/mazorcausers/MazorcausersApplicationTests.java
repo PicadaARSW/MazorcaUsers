31c9b0cb0fb1c827a78fca9e21b61cffb8ae8a9d
SebastianCardona-P <sebastian.cardona-p@mail.escuelaing.edu.co>
2025-04-25 21:23:09 -0500

    ADD:deploy

--- src/test/java/arsw/wherewe/back/mazorcausers/MazorcausersApplicationTests.java
+++ src/test/java/arsw/wherewe/back/mazorcausers/MazorcausersApplicationTests.java
@@ -2,9 +2,12 @@ package arsw.wherewe.back.mazorcausers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@TestPropertySource(properties = {"MONGODB_URI=mongodb://localhost:27017/testdb"})
 class MazorcausersApplicationTests {
 
 
